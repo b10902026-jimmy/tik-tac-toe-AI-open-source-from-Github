489ab8dbdb0a6e9383cfd4bc8b6de435b4600058
b10902026-jimmy <b10902026@mail.ntust.edu.tw>
2023-12-26 06:39:56 +0000

    setup_virtualenv.sh

--- setup_virtualenv.sh
+++ setup_virtualenv.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
-set -e # 如果任何語句的執行結果不是true則立即退出腳本
+set -e # 如果任何命令執行失敗則立即終止腳本
 
-# 檢查是否已安裝 pipenv
+# 檢查系統上是否已安裝 pipenv
 if ! command -v pipenv &> /dev/null
 then
-    echo "pipenv could not be found on the system, now installing pipenv..."
+    echo "系統未安裝 pipenv，現在開始安裝..."
     sudo apt-get update
     sudo apt-get install -y python3.8 python3-pip
 
     # 安裝 pipenv
     pip3 install pipenv
 
-    # 將 pipenv 安裝路徑添加到當前 session 的 PATH
+    # 將 pipenv 安裝路徑添加到當前會話的 PATH 中
     PIPENV_PATH=$(python3 -m site --user-base)/bin
     export PATH="$PIPENV_PATH:$PATH"
 else
-    echo "pipenv is already installed"
+    echo "pipenv 已經安裝"
 fi
 
 # 使用 pipenv 和 Python 3.8 創建虛擬環境
-echo "Using Python 3.8 to create virtual environment..."
+echo "正在使用 Python 3.8 創建虛擬環境..."
 pipenv --python 3.8
 
-# 安裝依賴項
-echo "Installing dependencies from Pipfile..."
+# 從 Pipfile 安裝依賴
+echo "正在從 Pipfile 安裝依賴..."
 pipenv install
 
-# 啟動虛擬環境並運行 tic-tac-toe-AI.py
-echo "Running tic-tac-toe-AI.py inside virtual environment..."
+# 在虛擬環境中啟動並運行 tic-tac-toe-AI.py
+echo "在虛擬環境中運行 tic-tac-toe-AI.py..."
 pipenv run python tic-tac-toe-AI.py
 
+echo "將 '$HOME/.local/bin' 添加到 PATH 中" >> ~/.bashrc
 
-
-echo "export PATH=\"$HOME/.local/bin:$PATH\"" >> ~/.bashrc
-
-echo "Setup is complete. Please close and reopen your terminal or run 'source ~/.bashrc' to apply PATH changes."
+echo "設置完成。請關閉並重新開啟您的終端機，或執行 'source ~/.bashrc' 來應用 PATH 變更。"
